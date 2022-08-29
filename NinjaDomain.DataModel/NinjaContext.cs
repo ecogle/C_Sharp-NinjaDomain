7c9244a6b8e6adc40beb78a03c14f6ba9f6382df
Chad Ogle <ecogle@gmail.com>
2022-08-29 13:32:19 -0500

    First Commit

---
+++ NinjaDomain.DataModel/NinjaContext.cs
@@ -0,0 +1,13 @@
+﻿using NinjaDomain.Classes;
+using System.Data.Entity;
+
+namespace NinjaDomain.DataModel
+{
+    public class NinjaContext :DbContext
+    {
+        //Explore Repository Pattern
+        public DbSet<Ninja> Ninjas { get; set; }
+        public DbSet <Clan> Clans { get; set; }
+        public DbSet<NinjaEquipment> Equipment { get; set; }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+https://app.pluralsight.com/course-player?clipId=61ca4667-f25e-4e37-82d4-1cea9613cdea
+
+Explore:
+	Repository Pattern
